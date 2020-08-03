1ac85508e9dfedcabd09eda23429b1e1f306aa1f
lizenghui <954267393@qq.com>
2020-08-03 14:17:44 +0800

    first commit

---
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -0,0 +1,136 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.service.AlphaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.imageio.IIOException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+@Controller
+@RequestMapping("/alpha")
+public class AlphaController {
+
+    @Autowired
+    private AlphaService alphaService;
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String sayHello() {
+        return "Hello Spring Boot";
+    }
+
+    @RequestMapping("/data")
+    @ResponseBody
+    public String getData() {
+        return alphaService.find();
+    }
+
+    @RequestMapping("/http")
+    public void http(HttpServletRequest request, HttpServletResponse response) {
+        // 获取请求数据
+        System.out.println(request.getMethod());
+        System.out.println(request.getServletPath());
+        Enumeration<String> enumeration = request.getHeaderNames();
+        while (enumeration.hasMoreElements()) {
+            String name = enumeration.nextElement();
+            String value = request.getHeader(name);
+            System.out.println(name + ": " + value);
+        }
+        System.out.println(request.getParameter("code"));
+
+        // 返回响应数据
+        response.setContentType("text/html;charset=utf-8");
+        try (
+                PrintWriter writer = response.getWriter();
+        ) {
+            writer.write("<h1>牛客网</h1>" + request.getParameter("code"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // GET 请求
+    // students?current=1&limit=20
+    @GetMapping("/students")
+    @ResponseBody
+    public String getStudents(
+            @RequestParam(name = "current", required = false, defaultValue = "1") int current,
+            @RequestParam(name = "limit", required = false, defaultValue = "10") int limit) {
+        System.out.println(current);
+        System.out.println(limit);
+        return "some students";
+    }
+    // students/123
+    @GetMapping("/students/{id}")
+    @ResponseBody
+    public String getStudent(@PathVariable("id") int id) {
+        System.out.println(id);
+        return "a student";
+    }
+
+    // Post请求
+    @PostMapping("/student")
+    @ResponseBody
+    public String saveStudent(String name, int age) {
+        System.out.println(name);
+        System.out.println(age);
+        return "success";
+    }
+
+    // 响应html数据
+    @GetMapping("/teacher")
+    public ModelAndView getTeacher() {
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("name", "张三");
+        mav.addObject("age", 30);
+        mav.setViewName("/demo/view");
+        return mav;
+    }
+
+    @GetMapping("/school")
+    public String getSchool(Model model) {
+        model.addAttribute("name", "北京大学");
+        model.addAttribute("age", 80);
+        return "/demo/view";
+    }
+
+    // 响应JSON数据(异步请求)
+    // Java对象 -> JSON字符串 -> JS对象
+    @GetMapping("/emp")
+    @ResponseBody
+    public Map<String, Object> getEmp() {
+        Map<String, Object> emp = new HashMap<>();
+        emp.put("name", "张三");
+        emp.put("age", 30);
+        emp.put("salary", 8000.00);
+        return emp;
+    }
+
+    @GetMapping("/emps")
+    @ResponseBody
+    public List<Map<String, Object>> getEmps() {
+        List<Map<String, Object>> list = new ArrayList<>();
+        Map<String, Object> emp = new HashMap<>();
+        emp.put("name", "张三");
+        emp.put("age", 30);
+        emp.put("salary", 8000.00);
+        list.add(emp);
+
+        emp = new HashMap<>();
+        emp.put("name", "李四");
+        emp.put("age", 30);
+        emp.put("salary", 500.00);
+        list.add(emp);
+
+        return list;
+    }
+
+}
---
+++ src/main/java/com/nowcoder/community/dao/DiscussPostMapper.java
@@ -0,0 +1,18 @@
+package com.nowcoder.community.dao;
+
+import com.nowcoder.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    // @Param("userId")用于给参数取别名
+    // 需要用到动态SQL时，并且方法有且只有一个参数，并且在<if>中使用，那就必须取别名
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+}
